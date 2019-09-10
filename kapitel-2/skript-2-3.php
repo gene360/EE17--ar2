bbaa5180e3aa37be2a5baa0df165c64650299997
Gene Helli <gene.helli@learnet.se>
2019-09-10 14:36:40 +0200

    övat på if satser, formulär  och variabler

---
+++ kapitel-2/skript-2-3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Uppgift 2-3</title>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<?php
+/* Ta emot data som skickas */
+   $bgfärg = $_REQUEST["bgfärg"];
+
+/* Öndra bakgrundsfärg */
+echo "<body style=\"background: $bgfärg\">";
+
+?>
+<body 
+<h1>Bekräftelse</h1>'<div class="kontainer">'
+   </div>
+   
+       
+</body>
+</html>
---
+++ kapitel-2/skript-2-6b.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Uppgift 2-6b</title>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Bekräftelse</h1>
+    <div class="kontainer">
+        <?php
+        /* Ta emot data som skickas */
+        $temp = $_REQUEST["temp"];
+        $riktning = $_REQUEST["riktning"];
+
+        switch ($riktning) {
+            case "CF":
+                /* Fahrenheit: F - (9/5)*C + 32 */
+            $fahrenheit = (9 / 5 * $temp + 32);
+            echo "<p>$temp&deg; Celsius är $fahrenheit&deg; Fahrenheit.</p>";
+                break;
+            case "CK":
+                /* Kelvin: K = C - 273 */
+            $kelvin = $temp - 273;
+            echo "<p>$temp&deg; Celsius är $kelvin&deg; Kelvin.</p>";
+                break;
+            case "FC":
+                /* Celcius: C - (F - 32)* (5/9)*/
+            $celsius = ($temp - 32) * (5 / 9);
+            echo "<p>$temp&deg; Farhrenheit är $celsius&deg; Celius.</p>";
+                break;
+        }
+        ?>
+    </div>
+</body>
+</html>
